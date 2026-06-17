2b26989d33cffb3cff52077832ff06800183d5d1
Nihal Gowda G Y <nihalgowda57@gmail.com>
2026-06-17 16:28:52 +0530

    feat : fixed deprecated datetime

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime
-from datetime import datetime
+from datetime import datetime, timezone
 from src.database import Base
 
 class QueryLog(Base):
@@ -8,6 +8,7 @@ class QueryLog(Base):
     id = Column(Integer, primary_key=True, index=True)
     query_text = Column(String, nullable=False)
     generated_response = Column(String, nullable=False)
-    context_found = Column(Boolean, default=True, index=True) 
-    latency_seconds = Column(Float, nullable=False)            
-    timestamp = Column(DateTime, default=datetime.utcnow, index=True)
+    context_found = Column(Boolean, default=True, index=True)
+    latency_seconds = Column(Float, nullable=False)
+    
+    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc), index=True)
